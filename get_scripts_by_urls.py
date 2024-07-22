1540ece6cc3b18c81cfab894ca67871ddf4b254f
Ilya Lasy <ilyalas@gmail.com>
2024-07-22 13:57:57 +0300

    experiments with ocr - gpt4o works the best

--- get_scripts_by_urls.py
+++ get_scripts_by_urls.py
@@ -1,32 +1,75 @@
 import json
+import os
 import time
 from pathlib import Path
 
 from tqdm import tqdm
 
 import sources
+from clean_files import clean_script
 from sources.utilities import format_filename
 
 DIR = Path("scripts") / "downloads"
 DIR.mkdir(parents=True, exist_ok=True)
+
+PRIORITY = [
+    "awesomefilm",
+    "screenplays",
+    "dailyscript",
+    "sfy",
+    "imsdb",
+    "scriptslug",
+    "scriptsavant",
+    "scriptpdf",
+]
 if __name__ == "__main__":
-    with open("urls.json", "r") as f:
+    with open("movies.json", "r") as f:
         data = json.load(f)
 
+    new_data = {k: v for k, v in data.items() if v.get("sources")}
+    print(f"No sources found for {len(data)-len(new_data)} scripts")
+    data = new_data
+    # new_data = {
+    #     k: v
+    #     for k, v in data.items()
+    #     if not (DIR / (format_filename(k) + ".txt")).exists()
+    # }
+    # print(f"Scripts already downloaded for {len(data)-len(new_data)} scripts")
+    data = new_data
+
+    failed = 0
     starttime = time.time()
     for name, d in tqdm(data.items()):
-        script_url = d.get("script_url")
-        if script_url:
-            source = d["source"]
-            file_name = format_filename(name)
-            if (DIR / (file_name + ".txt")).exists():
-                continue
-            text = sources.get_download_from_url_func(source)(script_url, file_name)
-            if not text:
-                print("Script is empty: " + script_url)
-                continue
-            with open(DIR / (file_name + ".txt"), "w") as f:
-                f.write(text)
+        file_name = format_filename(name)
+        save_name = DIR / (file_name + ".txt")
+        if save_name.exists():
+            with open(save_name, "r") as f:
+                text = f.read()
+                text = clean_script(text).strip()
+                if text:
+                    continue
+                else:
+                    os.remove(save_name)
 
-    print()
+        sources_list = {s["source"]: s["url"] for s in d.get("sources", [])}
+        sources_list = [
+            {"source": x, "url": sources_list[x]} for x in PRIORITY if x in sources_list
+        ]  # sort by priority
+        text_found = False
+        for source in sources_list:
+            script_url = source.get("url")
+            source = source.get("source")
+            if script_url and source:
+                text = sources.get_download_from_url_func(source)(script_url, file_name)
+                text = clean_script(text).strip()
+                if text:
+                    with open(save_name, "w") as f:
+                        f.write(text)
+                    text_found = True
+                    break
+        if not text_found:
+            print("Script is empty: " + script_url)
+            failed += 1
+
+    print(f"Failed: {failed}")
     print("Time taken = {} seconds".format(time.time() - starttime))
--- sources/utilities.py
+++ sources/utilities.py
@@ -1,12 +1,19 @@
 import os
+
+import openai
+import config
+os.environ["TESSDATA_PREFIX"] = config.TESSDATA_PREFIX
 import re
 import string
 import urllib.request
 
+import pymupdf
 import textract
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
-
+from tenacity import stop_after_attempt  # for exponential backoff
+from tenacity import retry, wait_random_exponential
+import base64
 
 def format_filename(s):
     valid_chars = "-() %s%s%s" % (string.ascii_letters, string.digits, "%")
@@ -34,6 +41,57 @@ def get_soup(url):
         soup = None
     return soup
 
+def clean_pdf_text(text):
+    text = text.encode('utf-8', 'ignore').decode('utf-8').strip()
+    text = text.replace("", "")
+    text = text.replace("•", "")
+    text = text.replace("·", "")
+    return text
+
+client = openai.OpenAI(
+    api_key=config.OPENAI_API_KEY
+)
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return client.chat.completions.create(**kwargs)
+
+# import easyocr
+# print("Loading OCR engine...")
+# reader = easyocr.Reader(['en']) 
+# print("OCR engine loaded.")
+# def ocr_image(image_bytes):
+#     reader.readtext(image_bytes,paragraph=True)
+#     result = reader.readtext(image_bytes,paragraph=True)
+#     return "\n".join([l[1] for l in result]) 
+
+
+def ocr_image(
+    image_bytes,    
+):    
+    base64_image = base64.b64encode(image_bytes).decode('utf-8')
+    response = completion_with_backoff(
+        model="gpt-4o",
+        temperature=0,        
+        messages=[
+            {
+                "role": "system",
+                "content": "Read the text in the image and return it verbatim.",
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            },
+        ],
+    )
+    return response.choices[0].message.content.strip()
+
 
 def get_pdf_text(url, name):
     doc = os.path.join("scripts", "temp", name + ".pdf")
@@ -41,13 +99,27 @@ def get_pdf_text(url, name):
     f = open(doc, "wb")
     f.write(result.read())
     f.close()
+    ocr = False
     try:
-        text = textract.process(doc, encoding="utf-8").decode("utf-8")
+        doc = pymupdf.open(doc)
+        text = ""
+        for page_num in range(doc.page_count):
+            page = doc.load_page(page_num)
+            page_text = clean_pdf_text(page.get_text())
+            if not page_text:
+                img = page.get_pixmap()
+                page_text = ocr_image(img.tobytes())                
+                ocr = True
+
+            text += page_text
+
     except Exception as err:
         print(err)
         text = ""
     # if os.path.isfile(doc):
     #     os.remove(doc)
+    if ocr:
+        print(f"{name} was OCR'd - there might be errors!")
     return text
 
 
